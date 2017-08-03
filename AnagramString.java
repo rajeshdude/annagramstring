40a3b790ce5fbacb8702fd215b3be05d95bc6b77
rajeshdude <rajeswarraod1993@gmail.com>
2017-08-03 12:00:06 +0530

    Create AnagramString.java

---
+++ AnagramString.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class AnagramString
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner input = new Scanner(System.in);
+    String text = input.next();   
+	String pattern = input.next();
+    anagram(text,pattern);
+    System.out.println("Hello World");
+	}
+	
+	private static boolean comp(int[] intext , int[] inpath){
+	for(int i = 0; i < intext.length; i++){
+		if(intext[i] != inpath[i]){
+			return false;
+		}
+	}
+	return true;
+    }
+
+    public static void anagram(String text, String pattern){
+    	int count = 0;
+    	int n = text.length();
+    	int m = pattern.length();
+    	
+    	if(n < m){
+    	 System.out.println("pattern length is more than text length");	
+    	}
+    		
+    	int itext[] = new int[256]; 
+    	int ipath[] = new int[256];
+    	int i = 0;
+    	for(i = 0; i < m; i++){
+    		ipath[pattern.charAt(i)]++;
+    		itext[text.charAt(i)]++;
+    	}
+    	do{
+    		if(comp(itext, ipath)){
+    			System.out.println("True");
+    			break;
+    		}
+    		itext[text.charAt(i)]++;
+    		itext[text.charAt(i-m)]--;
+    	} while(++i < n);
+    }
+	
+}
